5575eaf3d7388a76f25a4f709f5ab8f0c7fe669b
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2018-10-11 09:54:25 +0200

    uncomment test

--- vcls/controllers/controllers.py
+++ vcls/controllers/controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class MyModule(http.Controller):
-#     @http.route('/my_module/my_module/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class MyModule(http.Controller):
+    @http.route('/vcls/benefit/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/my_module/my_module/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('my_module.listing', {
-#             'root': '/my_module/my_module',
-#             'objects': http.request.env['my_module.my_module'].search([]),
-#         })
+    @http.route('/vcls/benefit/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('benefit.listing', {
+            'root': '/vcls/benefit',
+            'objects': http.request.env['vcls.benefit'].search([]),
+        })
 
-#     @http.route('/my_module/my_module/objects/<model("my_module.my_module"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('my_module.object', {
-#             'object': obj
-#         })
+    @http.route('/vcls/benefit/objects/<model("vcls.benefit"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('benefit.object', {
+            'object': obj
+        })
